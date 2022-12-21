8844c9f9b64e0197a5e2b946d267c916c5e2ee59
juliendy <julien.yasar@gmail.com>
2022-12-21 07:07:34 +0100

    refactor: new properties, add shadow placeholder props

--- src/lib/components/Travel/lighting/Lights.svelte
+++ src/lib/components/Travel/lighting/Lights.svelte
@@ -1,12 +1,40 @@
 <script>
 	import { T } from '@threlte/core';
-	const dir = {
-		castShadow: true,
-		position: [3, 10, 10],
-		intensity: 1
-	};
 </script>
 
-<T.DirectionalLight {...dir} />
-<T.DirectionalLight position={[3, 10, -10]} intensity={0.3} />
-<T.AmbientLight intensity={0.3} />
+<T.DirectionalLight
+	{...{
+		position: [3, 10, 10],
+		intensity: 1,
+		castShadow: true
+		//
+		// These shadow properties result in: `shadow.getFrameExtents is not a function`
+		//
+		// shadow: {
+		// 	mapSize: [1024, 1024],
+		// 	camera: {
+		// 		left: -5,
+		// 		top: 5,
+		// 		right: 5,
+		// 		bottom: -5,
+		// 		near: 0.5,
+		// 		far: 1000
+		// 	},
+		// 	bias: 0,
+		// 	radius: 1
+		// }
+	}}
+/>
+
+<T.DirectionalLight
+	{...{
+		position: [3, 10, -10],
+		intensity: 0.3
+	}}
+/>
+
+<T.AmbientLight
+	{...{
+		intensity: 0.3
+	}}
+/>
