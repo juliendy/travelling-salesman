af68dcad5a912429dec3c71647fb517ce7ad1740
juliendy <julien.yasar@gmail.com>
2022-12-22 13:12:48 +0100

    update: font variables+remove style attr+fix vertical sizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,13 +21,9 @@
 	}
 
 	main {
-		/* flex: 1;
 		display: flex;
-		flex-direction: column;
-		padding: 1rem; */
 		width: 100%;
-		/* max-width: 64rem;
-		margin: 0 auto; */
 		box-sizing: border-box;
+		flex: 1;
 	}
 </style>
