535f6d4b938b2884e7cb563285960bd445434510
juliendy <julien.yasar@gmail.com>
2022-12-22 07:02:27 +0100

    feat: init button component

---
+++ src/lib/components/UI/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let label = 'Label';
+	export let icon = '';
+</script>
+
+<button>
+	<span>
+		{label}
+	</span>
+	{#if icon.length > 0}
+		<img src={icon} alt="Icon" />
+	{/if}
+</button>
+
+<style>
+	button {
+		all: unset;
+		position: relative;
+		background-color: white;
+		border: 2px solid black;
+		padding: 1rem;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 0.5rem;
+	}
+	button::after {
+		content: '';
+		position: absolute;
+		display: block;
+		width: 100%;
+		height: 100%;
+		top: 0.5rem;
+		left: 0.5rem;
+		background-color: #f4b71f;
+		z-index: -1;
+	}
+	span {
+		font-weight: 700;
+	}
+	img {
+		height: 1rem;
+	}
+</style>
