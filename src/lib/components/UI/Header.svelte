0e033984f8312c9550a60bd59282a697ac55d330
juliendy <julien.yasar@gmail.com>
2022-12-24 05:03:42 +0100

    refactor: renamed and re-done score components

--- src/lib/components/UI/Header.svelte
+++ src/lib/components/UI/Header.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Nav from '$lib/components/UI/Nav.svelte';
-	import Score from '$lib/components/UI/Score.svelte'
+	import Sales from './Sales.svelte';
+	
 	import iconBurger from '$lib/images/icons/icon-burger.svg';
 
 	let showMenu = false;
@@ -8,7 +9,7 @@
 </script>
 
 <header>
-	<Score />
+	<Sales />
 	<h2><strong>Under Construction</strong></h2>
 	<button on:click={handleClick}><img src={iconBurger} alt="Burger" /></button>
 	<Nav {showMenu} on:click={handleClick} />
---
+++ src/lib/components/UI/Sales.svelte
@@ -0,0 +1,40 @@
+<script>
+	import iconRestart from '$lib/images/icons/icon-restart.svg';
+	export let sales = 0;
+</script>
+
+<div>
+	<div class="sales">👱🏽‍♂️ Sales: {sales} 💰</div>
+	<div class="restart">
+		<button on:click>
+			<img src={iconRestart} alt="Restart" />
+			<span>Restart</span>
+		</button>
+	</div>
+</div>
+
+<style>
+	div {
+		position: relative;
+	}
+	.sales {
+		font-family: 'IBM Plex Mono', monospace;
+		font-weight: 700;
+		text-transform: uppercase;
+	}
+	.restart {
+		position: absolute;
+		top: 2rem;
+	}
+	.restart > button {
+		all: unset;
+		background-color: black;
+		color: white;
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		padding: 0.5rem 1rem;
+		font-weight: 500;
+		text-transform: uppercase;
+	}
+</style>
