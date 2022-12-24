3ebe3c52b0bb527fbebb2293e10420c9924b31f7
juliendy <julien.yasar@gmail.com>
2022-12-24 10:50:14 +0100

    add: restart function + dynamically display comp based on state

--- src/lib/components/UI/Header.svelte
+++ src/lib/components/UI/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Nav from '$lib/components/UI/Nav.svelte';
 	import Sales from './Sales.svelte';
-	
+
 	import iconBurger from '$lib/images/icons/icon-burger.svg';
 
 	let showMenu = false;
@@ -9,10 +9,22 @@
 
 	// import stores
 	import { start, sales, positionSalesman, currentHouse, finish } from '$lib/stores/store';
+
+	const restartHandle = () => {
+		start.set(false);
+		finish.set(false);
+		sales.set(0);
+		positionSalesman.set(-14);
+		currentHouse.set(0);
+	};
 </script>
 
 <header>
-	<Sales />
+	{#if $start}
+		<Sales on:click={restartHandle} sales={$sales} />
+	{:else}
+		<div class="empty-placeholder" />
+	{/if}
 	<h2><strong>Under Construction</strong></h2>
 	<button on:click={handleClick}><img src={iconBurger} alt="Burger" /></button>
 	<Nav {showMenu} on:click={handleClick} />
@@ -32,6 +44,6 @@
 		background-color: #739368dd;
 	}
 	button {
-		all: unset
+		all: unset;
 	}
 </style>
