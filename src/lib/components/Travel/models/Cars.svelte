1b6254083c48ae30ee899018800f908637d9ae55
juliendy <julien.yasar@gmail.com>
2022-12-27 13:23:06 +0100

    feat: add new geo (cars) + init component

---
+++ src/lib/components/Travel/models/Cars.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { GLTF } from '@threlte/extras';
+	export let scale = 0.01;
+	export let position = { x: 0, y: 0, z: 1.25 };
+	const url = '/geometry/cars/car.glb';
+	// props
+	const gltf = {
+		url: url,
+		scale: scale,
+		position: position,
+		castShadow: true
+	};
+</script>
+
+<GLTF {...gltf} />
