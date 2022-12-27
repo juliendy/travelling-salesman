5cb3fc3592fa7b763549c5ce7e3eefe49c07a83d
juliendy <julien.yasar@gmail.com>
2022-12-27 06:54:16 +0100

    add: new models + update model pos + new favico

--- src/lib/components/Travel/Travel.svelte
+++ src/lib/components/Travel/Travel.svelte
@@ -7,7 +7,6 @@
 
 	// geometry
 	import Cube from '$lib/components/Travel/utils/Cube.svelte';
-	// import Floor from '$lib/components/Travel/utils/Floor.svelte';
 	import TreeSquare from '$lib/components/Travel/models/TreeSquare.svelte';
 	import BushSquare from './models/BushSquare.svelte';
 	import House from './models/House.svelte';
@@ -18,7 +17,7 @@
 	import Lights from '$lib/components/Travel/lighting/Lights.svelte';
 
 	// utilities
-	import Helpers from './utils/Helpers.svelte';
+	// import Helpers from './utils/Helpers.svelte';
 	import Background from '$lib/components/Travel/utils/Background.svelte';
 
 	// tweened stores
@@ -30,26 +29,38 @@
 	<Canvas rendererParameters={{ antialias: true }}>
 		<!-- Cameras -->
 		<Ortho />
+		
 		<!-- Lighting -->
 		<Lights />
+
 		<!-- Geometry -->
-		<!-- <Floor /> -->
 		<Cube />
+		<House position={{ x: -20, y: 0, z: -6 }} />
 		<House position={{ x: -8, y: 0, z: -6 }} />
 		<House position={{ x: 8, y: 0, z: -6 }} />
 		<House position={{ x: 16, y: 0, z: -6 }} />
+		<House position={{ x: 32, y: 0, z: -6 }} />
+
 		<T.Group position.x={$positionSalesman}>
 			<Salesman />
 		</T.Group>
+
 		<TreeSquare variant="l1" position={{ x: 2, y: 0, z: -7 }} />
+		<TreeSquare variant="s1" position={{ x: -15, y: 0, z: -10 }} />
 		<TreeSquare variant="s1" position={{ x: -4, y: 0, z: -4 }} />
 		<TreeSquare variant="s2" position={{ x: 7, y: 0, z: -9 }} />
+		<TreeSquare variant="l1" position={{ x: 27, y: 0, z: -8 }} />
+		<TreeSquare variant="s2" position={{ x: 24, y: 0, z: -5 }} />
+
 		<BushSquare variant="l1" position={{ x: 4, y: 0, z: 7 }} />
+		<BushSquare variant="l1" position={{ x: -17, y: 0, z: -4 }} />
 		<BushSquare variant="s1" position={{ x: 3, y: 0, z: -4 }} />
+		<BushSquare variant="s1" position={{ x: -7, y: 0, z: -4 }} />
 		<BushSquare variant="s2" position={{ x: 8, y: 0, z: -4 }} />
+
 		<Street />
 		<!-- Utils -->
-		<Helpers />
+		<!-- <Helpers /> -->
 		<Background color={'#739368'} />
 		<ShadowPlane />
 	</Canvas>
--- src/lib/components/Travel/models/Salesman.svelte
+++ src/lib/components/Travel/models/Salesman.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { GLTF } from '@threlte/extras';
 	export let scale = 0.01;
-	export let position = { x: 0, y: 0, z: 0 };
+	export let position = { x: 0, y: 0, z: -1.5 };
 	const url = '/geometry/characters/salesman.glb';
 	// props
 	const gltf = {
